852fb198c90c23257c34bb0cbe597441d81dffb3
Brendon Cardoso <brendoncarvalho22@gmail.com>
2020-12-19 22:16:59 -0300

    ajust login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,1 +1,9 @@
+@section('css')
+    <style>
+        .icheck-primary, div.card-footer p.my-0:nth-child(1){
+            display: none;  
+        }
+    </style>
+@endsection
+
 @extends('adminlte::auth/login')
